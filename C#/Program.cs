623c544f10cdfadc28193aab77d733ffba2e60b5
lamiyarahman92 <lamiyarahman92@gmail.com>
2021-02-01 10:10:16 +0600

    Basic Operation

---
+++ C#/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BASIC_OPERATION
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello World");
+            int a = 10;
+            Console.WriteLine("The variable is" + a);
+            float f = 10.678f;
+            Console.WriteLine("The variable of f id{0}", f);
+        }
+    }
+}
