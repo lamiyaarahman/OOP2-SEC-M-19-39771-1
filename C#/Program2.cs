f464ec6b7d1a87ca256859eee40fa44f307037ed
lamiyarahman92 <lamiyarahman92@gmail.com>
2021-02-08 10:37:10 +0600

    Lab 3

---
+++ C#/Program2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labtask3_P4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumOfOdd = 0;
+            int sumOfEven = 0;
+
+          
+                 for (int i = 1; i <= 100; i++)
+                    if (i % 2 == 0)
+                        sumOfEven += i;
+                    else
+                        sumOfOdd += i;
+
+            Console.WriteLine("Sum of Even Number: {0}\nSum of Odd Number: {1}", sumOfEven, sumOfOdd);
+
+
+
+
+        }
+    }
+}
+          
---
+++ C#/Program3.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labtask3_P8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          double phy, chem, bio, math, com;
+
+            Console.Write("Enter Physics Marks: ");
+            phy = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Chemistry Marks: ");
+            chem = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Biology Marks: ");
+            bio = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Mathematics Marks: ");
+            math = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Computer Marks: ");
+            com = Convert.ToDouble(Console.ReadLine());
+
+            double Marks = (phy + chem + bio + math + com) / 5;
+            string grade = "";
+
+            if (Marks >= 90)
+                grade = "A+";
+            else if (Marks < 90 && Marks >= 85)
+                grade = "A";
+            else if (Marks < 85 && Marks >= 80)
+                grade = "B+";
+            else if (Marks < 80 && Marks >= 75)
+                grade = "B";
+            else if (Marks < 75 && Marks >= 50)
+                grade = "C+";
+            else
+                grade = "F";
+
+            Console.WriteLine("\nThe Grade is: " + grade);
+
+
+            Console.ReadLine();
+        }
+    }
+}
+        
+    
+
