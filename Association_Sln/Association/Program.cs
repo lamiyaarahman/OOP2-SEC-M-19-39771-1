e5d320ce5d29ae1d9602f529f2b83fad245103e8
lamiyarahman92 <lamiyarahman92@gmail.com>
2021-03-01 11:21:55 +0600

    Labtask Association

--- Association_Sln/Association/Program.cs
+++ Association_Sln/Association/Program.cs
@@ -10,6 +10,32 @@ namespace Association
     {
         static void Main(string[] args)
         {
+            Student s1 = new Student("Tanvir", "12345", 2.5f);
+            Student s2 = new Student("Sabbir", "12346", 3.5f);
+            Student s3 = new Student("Rahim", "12347", 3.5f);
+
+
+            Department d1 = new Department("Computer Science", "CS");
+            Department d2 = new Department("Electrical Engineering", "EEE");
+           
+            s1.Dept = d1;
+            d1.AddStudent(s1,s2,s3);
+            
+            s2.Dept = d1;
+            s3.Dept = d2;
+            d2.AddStudent(s3);
+
+            s1.ShowDepartmentDetails();
+            s3.ShowDepartmentDetails();
+            Console.WriteLine("***********");
+            d1.Name = "Computer Science and Engineering";
+            d1.Id = "CSE";
+            s2.ShowDepartmentDetails();
+            Console.WriteLine("***********");
+            d1.PrintStudents();
+            s3.Name = "Abdur Rahim";
+            d2.PrintStudents();
+
         }
     }
 }
--- Association_Sln/Association/Student.cs
+++ Association_Sln/Association/Student.cs
@@ -8,5 +8,66 @@ namespace Association
 {
     class Student
     {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string id;
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        
+        private float cgpa;
+
+        public float Cgpa
+        {
+            get { return cgpa; }
+            set { cgpa = value; }
+        }
+        private Department dept;
+        public Department Dept
+        {
+            get {return dept;}
+            set { dept = value;}
+        }
+
+
+
+
+         public Student()
+        {
+            public Student(string name,string id,float cgpa)
+            {
+                this.name =name;
+                this.id = id;
+                this.cgpa=cgpa;
+
+       
+            }
     }
-}
+        public void ShowInfo()
+        {
+            Console.WriteLine("Name :" +name);
+            Console.WriteLine("Id :" +id);
+            Console.WriteLine("Cgpa :" +cgpa);
+        }
+
+        public void ShowDepartmentDetails()
+        {
+            ShowInfo();
+            dept.ShowInfo();
+        }
+
+
+
+
+    }
+
+
